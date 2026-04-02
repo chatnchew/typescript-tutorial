bb41f511520ccf35323ffb3fd94d0f0314da3a03
chatnchew <moonaluxe@gmail.com>
2026-04-02 16:23:14 -0500

    Type basics study

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,15 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-// character = 20;
-character = 'luigi';
-// age = 'yoshi';
-age = 40;
-// isBlackBelt = 'yes';
-isBlackBelt = true;
-var area = function (diameter) {
+"use strict";
+// variable declaration is the same as in JS 
+let character = 'mario';
+// 'number' type covers all kinds of numeric figures, including floats/decimals
+let age = 30;
+// boolean 
+let isBlackBelt = false;
+// once types are declared - either by inference or explicitly - they cannot be changed; this is the benefit of TS
+// ex - character = 30 // won't work because the type for this variable is string
+// character = 'luigi' // will work because this is another string
+const circ = (diameter) => {
     return diameter * Math.PI;
 };
-// console.log(area('hello'));
-console.log(area(7.5));
+// here we've explicitly set the type of the parameter - this means a number MUST be passed as the parameter for this function; it won't compile if the wrong type is given
+console.log(circ(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,19 +1,15 @@
+// variable declaration is the same as in JS 
 let character = 'mario';
+// 'number' type covers all kinds of numeric figures, including floats/decimals
 let age = 30;
+// boolean 
 let isBlackBelt = false;
-
-// character = 20;
-character = 'luigi';
-
-// age = 'yoshi';
-age = 40;
-
-// isBlackBelt = 'yes';
-isBlackBelt = true;
+// once types are declared - either by inference or explicitly - they cannot be changed; this is the benefit of TS
+  // ex - character = 30 // won't work because the type for this variable is string
+  // character = 'luigi' // will work because this is another string
 
 const circ = (diameter: number) => {
   return diameter * Math.PI;
 };
-
-// console.log(area('hello'));
+// here we've explicitly set the type of the parameter - this means a number MUST be passed as the parameter for this function; it won't compile if the wrong type is given
 console.log(circ(7.5));
