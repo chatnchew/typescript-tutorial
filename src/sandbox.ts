2fb9eac72c7ca3f1182def88dd993f5c06de6d48
chatnchew <moonaluxe@gmail.com>
2026-04-03 16:26:32 -0500

    learning function signatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,26 +1,31 @@
-// let greet: Function;
+// function signatures - defines what arguments it takes in and what type it returns
 
-// example 1
+// ex 1
 let greet: (a: string, b: string) => void;
-
+// this means that this function will take two strings and return a value with type void
+  // the parameters can be called whatever in the signature; they're moreso there to define the type
 greet = (name: string, greeting: string) => {
-  console.log(`${name} says ${greeting}`);
-}
+  console.log(`${name} says ${greeting}`)
+};
 
-// example 2
+// ex 2
 let calc: (a: number, b: number, c: string) => number;
 
 calc = (numOne: number, numTwo: number, action: string) => {
   if (action === 'add') {
-    return numOne + numTwo;
+    return numOne + numTwo
   } else {
-    return numOne - numTwo;
-  }
-}
+    // have to write the function so that whatever the return value is is a number, as specified
+    return numOne - numTwo
+  };
+};
 
-// example 3
+// ex 3
 let logDetails: (obj: {name: string, age: number}) => void;
 
-logDetails = (ninja: {name: string, age: number}) => {
+// using a type alias to fit the function signature
+type person = {name: string, age: number};
+
+logDetails = (ninja: person) => {
   console.log(`${ninja.name} is ${ninja.age} years old`);
-}
+};
