6e444471f43712b0a711d3979b70ad18f096da1d
chatnchew <moonaluxe@gmail.com>
2026-04-03 15:56:13 -0500

    type alias practice

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,23 +1,14 @@
-// let greet: Function = () => {
-//   console.log('hello, world');
-// }
+// type aliases can make it easier to deal with union types
+  // instead of 'uid: string | number' we can say 'uid: StringOrNum'
+type StringOrNum = string | number;
+// we can get even more complex, with the types for object entries being defined, and those types being other aliases
+type objWtihName = {name: string, uid: StringOrNum};
+// can simply use this type instead of 'user: { name: string; uid: StringOrNum }'
 
-// greet = 'hello';
+const logDeatails = (uid: StringOrNum, item: string) => {
+  console.log(`${item} has a uid of ${uid}`);
+};
 
-// greet = () => {
-//   console.log('hello, again');
-// }
-
-const add = (a: number, b: number, c/*?*/: number | string = 10): void => {
-  console.log(a + b);
-  console.log(c);
-}
-
-add(5, 10, 'ninja');
-
-const minus = (a: number, b: number): number => {
-  return a + b;
-}
-
-let result = minus(10,7);
-console.log(result);
+const greet = (user: objWtihName) => {
+  console.log(`${user.name} says hello`);
+};
