4a4d2e0256cf1efc531f1d064979d6ae940c42f6
chatnchew <moonaluxe@gmail.com>
2026-04-07 14:44:55 -0500

    How modules work

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
-import { Invoice } from './classes/Invoice.js';
+// utilizing the modules system from es6
+import { Invoice } from './classes/Invoice.js'; // defining the class in a separate file; use .js for TS files!
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 // invOne.client = 'yoshi';
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import { Invoice } from './classes/Invoice.js';
+// utilizing the modules system from es6
+import { Invoice } from './classes/Invoice.js'; // defining the class in a separate file; use .js for TS files!
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
@@ -13,8 +14,6 @@ invoices.push(invTwo);
 invoices.forEach(inv => {
   console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
 })
-
-
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.children);
