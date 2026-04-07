183a4341f00043d81f24ed3ba5eb09299444c641
chatnchew <moonaluxe@gmail.com>
2026-04-07 12:52:13 -0500

    Interacting with the DOM

--- public/app.js
+++ public/app.js
@@ -1,18 +1,22 @@
 "use strict";
-var anchor = document.querySelector('a');
-if (anchor) {
-    console.log(anchor.href);
-}
-console.log(anchor.href);
-//const form = document.querySelector('form')!;
-var form = document.querySelector('.new-item-form');
-console.log(form.children);
+// interacting with the DOM
+// const anchor = document.querySelector('a')!;
+// typescript will be wary about accessing object properties of DOM elements; this can be overriden with a ! as seen above or like so:
+// if(anchor) {
+//  console.log(anchor.href); }
+// console.log(anchor.href);
+// typescript will be able to pick up that an anchor tag was grabbed with this variable, and provide intellisense suggestions that are relevant
+// const form = document.querySelector('form')!; - this will grab the form element
+// const form = document.querySelector(".new-item-form")! - this grabs a generic element, as this is a class and not the specific form element
+// we can use typecasting to cast a certain type onto this class - this will explicitly make the type HTMLFormElement
+const form = document.querySelector(".new-item-form");
+// console.log(form.children);
 // inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+const type = document.querySelector('#type');
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); // prevent default refresh behavior as per usual
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,33 @@
-const anchor = document.querySelector('a')!;
-if(anchor) {
-  console.log(anchor.href);
-}
-console.log(anchor.href);
+// interacting with the DOM
 
-//const form = document.querySelector('form')!;
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
+// const anchor = document.querySelector('a')!;
+
+// typescript will be wary about accessing object properties of DOM elements; this can be overriden with a ! as seen above or like so:
+   // if(anchor) {
+   //  console.log(anchor.href); }
+
+// console.log(anchor.href);
+// typescript will be able to pick up that an anchor tag was grabbed with this variable, and provide intellisense suggestions that are relevant
+
+// const form = document.querySelector('form')!; - this will grab the form element
+// const form = document.querySelector(".new-item-form")! - this grabs a generic element, as this is a class and not the specific form element
+// we can use typecasting to cast a certain type onto this class - this will explicitly make the type HTMLFormElement
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+// console.log(form.children);
 
 // inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
 
 form.addEventListener('submit', (e: Event) => {
-  e.preventDefault();
+  e.preventDefault(); // prevent default refresh behavior as per usual
 
   console.log(
     type.value, 
-    tofrom.value, 
-    details.value, 
+    tofrom.value,
+    details.value,
     amount.valueAsNumber
-  );
+  )
 });
